659f2812ecef1a1b5f73f27934740fe445ccaab3
Sarhan <github@tux.tn>
2025-11-10 22:14:13 +0100

    fix(preinstall): use correct command for copying ndi headers on windows

--- scripts/preinstall.mjs
+++ scripts/preinstall.mjs
@@ -252,7 +252,7 @@ async function main() {
 			log.step("Assembling Windows NDI SDK subset");
 			shell.rm("-rf", "ndi");
 			shell.mkdir("-p", ["ndi/include", "ndi/lib/win-x86", "ndi/lib/win-x64"]);
-			shell.cp("-R", path.join(extractDir, "app/Include"), "ndi/include/");
+			shell.cp(path.join(extractDir, "app/Include/*"), "ndi/include/");
 			shell.cp(
 				path.join(extractDir, "app/Lib/x86/Processing.NDI.Lib.x86.lib"),
 				"ndi/lib/win-x86/Processing.NDI.Lib.x86.lib",
